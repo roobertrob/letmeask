a364403cd16e67f8e82f8628728cc39a0931dd63
Robert <robert.fonseca@solides.com.br>
2023-02-28 19:37:42 -0300

    test: implement tests for question component

--- src/components/Question/index.test.tsx
+++ src/components/Question/index.test.tsx
@@ -1,11 +1,26 @@
-import { render } from 'utils/tests';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { Question } from '.';
 
-describe('<Button />', () => {
+describe('<Question />', () => {
   it('should render correctly', () => {
     const { container } = render(
       <Question content="anything" author={{ avatar: '', name: '' }} />,
     );
     expect(container.firstChild).toMatchSnapshot();
+  });
+  it('should render with props', () => {
+    render(
+      <Question
+        content="anything"
+        author={{ avatar: 'https://avatar.com', name: 'John Doe' }}
+      />,
+    );
+    expect(screen.getByText(/anything/i)).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('alt', 'John Doe');
+    expect(screen.getByRole('img')).toHaveAttribute(
+      'src',
+      'https://avatar.com',
+    );
   });
 });
